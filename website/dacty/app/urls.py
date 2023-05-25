4ba5c0fe341d4aea130edead37019a9f0d4205ef
Quentin ERDINGER <91551241+EQ-0@users.noreply.github.com>
2023-05-25 17:14:10 +0200

    Clean: repository removed: [__PYCACHE__] folder && Add: register page

--- website/dacty/app/urls.py
+++ website/dacty/app/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, register
+from .views import home, register, login
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
-    path('register', register, name="register")
+    path('register', register, name="register"),
+    path('login', login, name="login")
 ]
--- website/dacty/app/views.py
+++ website/dacty/app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,10 +8,20 @@ def home(request):
     return render(request, "home.html")
 
 def register(request):
-    return render(request, "register.html")
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
+        if (User.objects.filter(username=username)).exists():
+            messages.info(request, "Username already exist")
+            return redirect(register)
+        else:
+            user = User.objects.create_user(username=username, password=password, email=email)
+            user.set_password(password)
+            user.save()
+            return redirect('login')
     else:
         return render(request, "register.html")
+
+def login(request):
+    return render(request, "login.html")
