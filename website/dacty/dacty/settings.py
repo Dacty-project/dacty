549766125febfbe5d6df13962dc7e5477df7a73a
Quentin ERDINGER <91551241+EQ-0@users.noreply.github.com>
2023-05-26 09:08:13 +0200

    Add: navbar in [home] page

--- website/dacty/dacty/settings.py
+++ website/dacty/dacty/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,6 @@ SECRET_KEY = 'django-insecure-a^djiqlua10(*gq(dw67dv742_-2up&t^xhju54o0r(1w%x_)w
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "public"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
