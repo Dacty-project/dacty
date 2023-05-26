21cf3b6192a9c806c310308c48f94009c903d221
Quentin ERDINGER <91551241+EQ-0@users.noreply.github.com>
2023-05-26 08:08:10 +0200

    Add: basic login/register/logout pages

--- website/dacty/app/urls.py
+++ website/dacty/app/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home, register, login
+from .views import home, register, login, logout
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
     path('register', register, name="register"),
-    path('login', login, name="login")
+    path('login', login, name="login"),
+    path('logout', logout, name="logout"),
 ]
--- website/dacty/app/views.py
+++ website/dacty/app/views.py
@@ -5,13 +5,19 @@ from django.contrib import messages
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    if request.user.is_authenticated:
+        return render(request, "home.html")
+    else:
+        return redirect('login')
 
 def register(request):
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
         password = request.POST['password']
+        if (len(username) < 5 or len(password) < 5):
+            messages.info(request, "User / Password length must be > 5")
+            return redirect('register')
         if (User.objects.filter(username=username)).exists():
             messages.info(request, "Username already exist")
             return redirect('register')
@@ -32,8 +38,12 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            return redirect('None')
+            return redirect('home')
         else:
             messages.info(request, 'Invalid User / Password')
             return redirect('login')
     return render(request, "login.html")
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
